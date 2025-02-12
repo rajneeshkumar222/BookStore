837b4bef9800f7e459fc2361ec0244e71e6a5f23
Rajneesh Kumar Maurya <144912920+rajneeshkumar222@users.noreply.github.com>
2025-02-12 23:02:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const cors = require("cors");
 // const cors = require('cors');
 
 const corsOptions = {
-    origin: "http://localhost:5173",  // Your frontend URL
+    origin: "*",  // Allows all origins
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization", "id", "bookid"],  // Add "bookid" here
+    allowedHeaders: ["Content-Type", "Authorization", "id", "bookid"],  
 };
+
 
 
 
